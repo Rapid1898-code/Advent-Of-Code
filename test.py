060ef3cb16e78c7d3ac00beeaff62dc7b156db8b
Rapid1898-code <57018730+Rapid1898-code@users.noreply.github.com>
2019-12-21 12:26:01 +0100

    kjlk

--- test.py
+++ test.py
@@ -1,9 +1,31 @@
-liste = [1100,2,1102]
+import itertools
 
-def inp():
-    inp = input("Input: ")
-    liste[liste[i+1]] = int(inp)
+combi =[]
+liste = [1,2,3,4,5,10]
 
-i = 0
-inp()
-print (liste)
+def make_combi():
+  perm = itertools.product(liste, repeat=2)
+  for i in perm:
+    if i[0] == i[1] and i[0] != "1": continue
+    if i[0] == "0" and i[1] != "1": continue
+    if i[1] == "0" and i[0] != "1": continue
+    combi.append([int(i[0]), int(i[1])])
+  combi.sort()
+
+make_combi()
+print (combi)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
