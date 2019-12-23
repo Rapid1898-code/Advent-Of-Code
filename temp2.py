7e51069c11d0506b9b3feff7409c9d0bd73da029
Rapid1898-code <57018730+Rapid1898-code@users.noreply.github.com>
2019-12-23 11:26:43 +0100

    kjlk

--- temp2.py
+++ temp2.py
@@ -1,6 +1,11 @@
-mydict = {}
-mydict[(2,2)] = 'AA'
-mydict[(4,5)] = 'DA'
+liste = []
 
-print(mydict)
-print(mydict[(2,2)])
+def read_input():
+    file = open("Advent20 Part1 Example01.txt", "r")
+    for line in file:
+        liste.append(line.rstrip('\n'))
+    file.close()
+
+read_input()
+for i in liste:
+    print (len(i))
