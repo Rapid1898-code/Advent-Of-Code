cc4929ecc8922c2b824a50941a472b472527fd12
Rapid1898-code <markuspolzer@gmx.at>
2020-01-07 23:00:03 +0100

    kjlk

--- Advent2015/2015_Advent19 Part2_TryBackwards.py
+++ Advent2015/2015_Advent19 Part2_TryBackwards.py
@@ -1,13 +1,10 @@
 import re
 
-with open('Advent19Input.txt') as f:
+with open('Advent19Input2.txt') as f:
     src = [x.strip().split() for x in f.readlines()]
 #src_str= 'CRnCaSiRnBSiRnFArTiBPTiTiBFArPBCaSiThSiRnTiBPBPMgArCaSiRnTiMgArCaSiThCaSiRnFArRnSiRnFArTiTiBFArCaCaSiRnSiThCaCaSiRnMgArFYSiRnFYCaFArSiThCaSiThPBPTiMgArCaPRnSiAlArPBCaCaSiRnFYSiThCaRnFArArCaCaSiRnPBSiRnFArMgYCaCaCaCaSiThCaCaSiAlArCaCaSiRnPBSiAlArBCaCaCaCaSiThCaPBSiThPBPBCaSiRnFYFArSiThCaSiRnFArBCaCaSiRnFYFArSiThCaPBSiThCaSiRnPMgArRnFArPTiBCaPRnFArCaCaCaCaSiRnCaCaSiRnFYFArFArBCaSiThFArThSiThSiRnTiRnPMgArFArCaSiThCaPBCaSiRnBFArCaCaPRnCaCaPMgArSiRnFYFArCaSiThRnPBPMgAr'
 #src_str= 'HOH'
 #src_str= 'HOHOHO'
-
-print(len(src_str))
-
 
 def find_all(str,search):
     result_temp = []
@@ -16,7 +13,10 @@ def find_all(str,search):
     return(result_temp)
 
 def check_mol():
-    results = {'CRnCaSiRnBSiRnFArTiBPTiTiBFArPBCaSiThSiRnTiBPBPMgArCaSiRnTiMgArCaSiThCaSiRnFArRnSiRnFArTiTiBFArCaCaSiRnSiThCaCaSiRnMgArFYSiRnFYCaFArSiThCaSiThPBPTiMgArCaPRnSiAlArPBCaCaSiRnFYSiThCaRnFArArCaCaSiRnPBSiRnFArMgYCaCaCaCaSiThCaCaSiAlArCaCaSiRnPBSiAlArBCaCaCaCaSiThCaPBSiThPBPBCaSiRnFYFArSiThCaSiRnFArBCaCaSiRnFYFArSiThCaPBSiThCaSiRnPMgArRnFArPTiBCaPRnFArCaCaCaCaSiRnCaCaSiRnFYFArFArBCaSiThFArThSiThSiRnTiRnPMgArFArCaSiThCaPBCaSiRnBFArCaCaPRnCaCaPMgArSiRnFYFArCaSiThRnPBPMgAr'}
+    #results = {'CRnCaSiRnBSiRnFArTiBPTiTiBFArPBCaSiThSiRnTiBPBPMgArCaSiRnTiMgArCaSiThCaSiRnFArRnSiRnFArTiTiBFArCaCaSiRnSiThCaCaSiRnMgArFYSiRnFYCaFArSiThCaSiThPBPTiMgArCaPRnSiAlArPBCaCaSiRnFYSiThCaRnFArArCaCaSiRnPBSiRnFArMgYCaCaCaCaSiThCaCaSiAlArCaCaSiRnPBSiAlArBCaCaCaCaSiThCaPBSiThPBPBCaSiRnFYFArSiThCaSiRnFArBCaCaSiRnFYFArSiThCaPBSiThCaSiRnPMgArRnFArPTiBCaPRnFArCaCaCaCaSiRnCaCaSiRnFYFArFArBCaSiThFArThSiThSiRnTiRnPMgArFArCaSiThCaPBCaSiRnBFArCaCaPRnCaCaPMgArSiRnFYFArCaSiThRnPBPMgAr'}
+    results = {'HOH'}
+    #results = {'HOHOHO'}
+
     count = 0
     while True:
         temp_result = set ()
@@ -25,18 +25,8 @@ def check_mol():
             for entry in src:
                 pos_list = find_all (result, entry[2])
                 for pos in pos_list:
-
-
-                    if len (entry[0]) == 1:
-                        res_str = result[:pos] + entry[2] + result[pos + 1:]
-                    elif len (entry[0]) == 2:
-                        res_str = result[:pos] + entry[2] + result[pos + 2:]
-                    else:
-                        print ('Error pos in pos_list')
-
-
-                    if res_str == src_str: return(count)
-                    if len(res_str) <= len(src_str): temp_result.add(res_str)
+                    res_str = result[:pos] + entry[0] + result[len(entry[2])+1:]
+                    if res_str == 'e': return(count)
         results = temp_result
 
 res=check_mol()
