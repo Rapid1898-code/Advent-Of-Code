2c8415000c51201b07b742445d7754711582ec4a
Rapid1898-code <markuspolzer@gmx.at>
2020-01-08 08:43:08 +0100

    kjlk

--- Advent2015/2015_Advent19 Part2_TryBackwards.py
+++ Advent2015/2015_Advent19 Part2_TryBackwards.py
@@ -1,6 +1,6 @@
 import re
 
-with open('Advent19Input2.txt') as f:
+with open('Advent19Input.txt') as f:
     src = [x.strip().split() for x in f.readlines()]
 #src_str= 'CRnCaSiRnBSiRnFArTiBPTiTiBFArPBCaSiThSiRnTiBPBPMgArCaSiRnTiMgArCaSiThCaSiRnFArRnSiRnFArTiTiBFArCaCaSiRnSiThCaCaSiRnMgArFYSiRnFYCaFArSiThCaSiThPBPTiMgArCaPRnSiAlArPBCaCaSiRnFYSiThCaRnFArArCaCaSiRnPBSiRnFArMgYCaCaCaCaSiThCaCaSiAlArCaCaSiRnPBSiAlArBCaCaCaCaSiThCaPBSiThPBPBCaSiRnFYFArSiThCaSiRnFArBCaCaSiRnFYFArSiThCaPBSiThCaSiRnPMgArRnFArPTiBCaPRnFArCaCaCaCaSiRnCaCaSiRnFYFArFArBCaSiThFArThSiThSiRnTiRnPMgArFArCaSiThCaPBCaSiRnBFArCaCaPRnCaCaPMgArSiRnFYFArCaSiThRnPBPMgAr'
 #src_str= 'HOH'
@@ -13,21 +13,33 @@ def find_all(str,search):
     return(result_temp)
 
 def check_mol():
-    #results = {'CRnCaSiRnBSiRnFArTiBPTiTiBFArPBCaSiThSiRnTiBPBPMgArCaSiRnTiMgArCaSiThCaSiRnFArRnSiRnFArTiTiBFArCaCaSiRnSiThCaCaSiRnMgArFYSiRnFYCaFArSiThCaSiThPBPTiMgArCaPRnSiAlArPBCaCaSiRnFYSiThCaRnFArArCaCaSiRnPBSiRnFArMgYCaCaCaCaSiThCaCaSiAlArCaCaSiRnPBSiAlArBCaCaCaCaSiThCaPBSiThPBPBCaSiRnFYFArSiThCaSiRnFArBCaCaSiRnFYFArSiThCaPBSiThCaSiRnPMgArRnFArPTiBCaPRnFArCaCaCaCaSiRnCaCaSiRnFYFArFArBCaSiThFArThSiThSiRnTiRnPMgArFArCaSiThCaPBCaSiRnBFArCaCaPRnCaCaPMgArSiRnFYFArCaSiThRnPBPMgAr'}
-    results = {'HOH'}
+    results = {'CRnCaSiRnBSiRnFArTiBPTiTiBFArPBCaSiThSiRnTiBPBPMgArCaSiRnTiMgArCaSiThCaSiRnFArRnSiRnFArTiTiBFArCaCaSiRnSiThCaCaSiRnMgArFYSiRnFYCaFArSiThCaSiThPBPTiMgArCaPRnSiAlArPBCaCaSiRnFYSiThCaRnFArArCaCaSiRnPBSiRnFArMgYCaCaCaCaSiThCaCaSiAlArCaCaSiRnPBSiAlArBCaCaCaCaSiThCaPBSiThPBPBCaSiRnFYFArSiThCaSiRnFArBCaCaSiRnFYFArSiThCaPBSiThCaSiRnPMgArRnFArPTiBCaPRnFArCaCaCaCaSiRnCaCaSiRnFYFArFArBCaSiThFArThSiThSiRnTiRnPMgArFArCaSiThCaPBCaSiRnBFArCaCaPRnCaCaPMgArSiRnFYFArCaSiThRnPBPMgAr'}
+    #results = {'HOH'}
     #results = {'HOHOHO'}
 
     count = 0
     while True:
         temp_result = set ()
         count += 1
+        print(len(results), count)
         for result in results:
             for entry in src:
                 pos_list = find_all (result, entry[2])
+
+                pos_list = pos_list[:1]
+
                 for pos in pos_list:
-                    res_str = result[:pos] + entry[0] + result[len(entry[2])+1:]
-                    if res_str == 'e': return(count)
-        results = temp_result
+                    res_str = result[:pos] + entry[0] + result[pos+len(entry[2]):]
+                    if entry[0] == 'e':
+                        if res_str == 'e': return(count)
+                        else: continue
+                    temp_result.add(res_str)
+
+        temp_result = list(temp_result)
+        temp_result.sort(key=len)
+        results = temp_result[:10]
+        results = set(results)
+
 
 res=check_mol()
 print(res)
--- Advent2015/test2.py
+++ Advent2015/test2.py
@@ -1,4 +1,39 @@
-import re
-m=[]
-[m.start() for m in re.finditer('test', 'test test test test')]
-print(m)
+import random
+
+start_molecule = "...molecule does here..."
+
+def day19_part1():
+    rules = []
+    for line in open('Advent19Input.txt'):
+        h, r = line.split(' => ')
+        rules.append((h, r.strip()))
+    new_molecules = set()
+    for head, rep in rules:
+        for i in xrange(len(start_molecule)):
+            if start_molecule[i:i+len(head)] == head:
+                m = start_molecule[:i] + rep + start_molecule[i+len(head):]
+                new_molecules.add(m)
+    print (len(new_molecules))
+
+def day19_part2():
+    rules = []
+    for line in open('Advent19Input.txt'):
+        h, r = line.split(' => ')
+        rules.append((h, r.strip()))
+
+    steps = 0
+    mol = start_molecule[:]
+    r = 0
+    while r < len(rules):
+        head, rep = rules[r]
+        i = mol.find(rep)
+        if i != -1:
+            steps += 1
+            mol = mol[:i] + head + mol[i+len(rep):]
+            r = 0
+            random.shuffle(rules)
+            if mol == 'e':
+                print (steps)
+                return
+        else:
+            r += 1
