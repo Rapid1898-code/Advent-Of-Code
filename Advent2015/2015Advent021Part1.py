89c2fd175708fb289b4a5674d8873f423b914f82
Rapid1898-code <57018730+Rapid1898-code@users.noreply.github.com>
2020-01-09 16:20:38 +0100

    Add files via upload

---
+++ Advent2015/2015Advent021Part1.py
@@ -0,0 +1,43 @@
+import itertools
+weapons = {1:(8,4,0), 1:(10,5,0), 3:(25,6,0), 4:(40,7,0),5:(74,8,0)}
+armors = {0:(0,0,0),1:(13,0,1),2:(31,0,2),3:(53,0,3),4:(75,0,4),5:(102,0,5)}
+rings = {0:(0,0,0),1:(25,1,0),2:(50,2,0),3:(100,3,0),4:(20,0,1),5:(40,0,2),6:(80,0,3)}
+enemy = (100,8,2)
+player_hp = 100
+min_gold = 9999999
+
+ring_kombis = [0,1,2,3,4,5,6]
+kombi = itertools.combinations([1,2,3,4,5,6],2)
+for i in kombi: ring_kombis.append(i)
+
+def fight(p_hp,p_dmg,p_arm,e_hp,e_dmg,e_arm):
+    turn = 'P'
+    while p_hp > 0 and e_hp > 0:
+        if turn == 'P':
+            player_hit = p_dmg - e_arm
+            if player_hit > 1: e_hp -= player_hit
+            else: e_hp -= 1
+            turn = 'E'
+        elif turn == 'E':
+            enemy_hit = e_dmg - p_arm
+            if enemy_hit > 1: p_hp -= enemy_hit
+            else: p_hp -= 1
+            turn = 'P'
+    if p_hp > 0: return ("WIN")
+    elif e_hp > 0: return ("LOOSE")
+    else: return ("DRAW")
+
+for w in weapons:
+    for a in range(6):
+        for r in ring_kombis:
+            if isinstance(r,int):
+                player_dmg = weapons.get(w)[1] + rings.get(r)[1]
+                player_arm = armors.get(a)[2]+ rings.get(r)[2]
+                ring_gold = weapons.get(w)[0] + armors.get(a)[0] + rings.get(r)[0]
+            else:
+                player_dmg = weapons.get(w)[1] + rings.get(r[0])[1] + rings.get(r[1])[1]
+                player_arm = armors.get(a)[2] + rings.get(r[0])[2] + rings.get(r[1])[2]
+                ring_gold = weapons.get(w)[0] + armors.get(a)[0] + rings.get(r[0])[0] + rings.get(r[1])[0]
+            erg = fight(player_hp, player_dmg, player_arm, enemy[0], enemy[1], enemy[2])
+            if erg == "WIN" and ring_gold < min_gold: min_gold = ring_gold
+print(min_gold)
--- Advent2015/test.py
+++ Advent2015/test.py
@@ -1,14 +1,13 @@
-set1 = {1,2,3}
-set2 = set()
-print(set1)
-set2.add(4)
-set2.add(5)
-set2.add(6)
-print(set2)
+import itertools
+# weapons = {'W1':(8,4,0), 'W2':(10,5,0), 'W3':(25,6,0), 'W4':(40,7,0),'W5':(74,8,0)}
+# for w in weapons:
+#     print(weapons.get(w))
 
-set1 = set2
-print(set1)
-set1.update
+ring_kombis = [0,1,2,3,4,5,6]
+kombi = itertools.combinations([1,2,3,4,5,6],2)
+for i in kombi: ring_kombis.append(i)
+print (ring_kombis)
+
 
 
 
