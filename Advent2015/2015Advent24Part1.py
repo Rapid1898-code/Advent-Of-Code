63595175a14016fac7fd250788105aa1b02cb321
Rapid1898-code <markuspolzer@gmx.at>
2020-01-12 22:29:19 +0100

    kjlkgfhdfh

--- Advent2015/2015Advent24Part1.py
+++ Advent2015/2015Advent24Part1.py
@@ -4,14 +4,88 @@ def SumTheList(thelist, target):
     arr = []
     p = []
     if len(thelist) > 0:
-        for r in range(0,len(thelist)+1):
+        for r in range(0,10):
             arr += list(itertools.combinations(thelist, r))
-
         for item in arr:
             if sum(item) == target:
                 p.append(item)
-
     return p
 
-erg = SumTheList([1,2,3,4,5,7,8,9,10,11], 20)
-print(erg)
+erg = SumTheList([1,3,5,11,13,17,19,23,29,31,41,43,47,53,59,61,67,71,73,79,83,89,97,101,103,107,109,113], 516)
+erg.sort(key=len)
+
+#erg = SumTheList([1,2,3,4,5,7,8,9,10,11], 20)
+#erg.sort(key=len)
+
+pos = 0
+length = len (erg[0])
+middle = erg[pos]
+left = []
+end_kombis = []
+temp_erg = [erg[pos]]
+for idx_kombi in range(pos+1,len(erg)):
+    find = True
+    for element in erg[idx_kombi]:
+        if element in middle:
+            find = False
+            break
+        if len(temp_erg) == 2:
+            if element in left:
+                find = False
+                break
+    if find == True:
+        temp_erg.append(erg[idx_kombi])
+        if len(temp_erg) == 2: left = list(erg[idx_kombi])
+    if len(temp_erg) == 3:
+        end_kombis.append(temp_erg)
+        break
+pos += 1
+
+while len(erg[pos]) == len(end_kombis[0][0]):
+    middle = erg[pos]
+    temp_erg = [erg[pos]]
+    for idx_kombi in range(pos+1,len(erg)-1):
+        find = True
+        for element in erg[idx_kombi]:
+            if element in middle: find = False
+            if len (temp_erg) == 2:
+                if element in left: find = False
+        if find == True:
+            temp_erg.append (erg[idx_kombi])
+            if len (temp_erg) == 2: left = list (erg[idx_kombi])
+        if len (temp_erg) == 3:
+            end_kombis.append (temp_erg)
+            break
+    pos += 1
+
+print(end_kombis)
+
+final_erg = []
+min_qe = 999999
+for idx in end_kombis:
+    temp_mlt = 1
+    for idx2 in idx[0]: temp_mlt *= idx2
+    if temp_mlt < min_qe:
+        min_qe = temp_mlt
+        final_erg = list(idx)
+
+print (final_erg)
+print (min_qe)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- Advent2015/test.py
+++ Advent2015/test.py
@@ -1,12 +1,8 @@
 import itertools
-# weapons = {'W1':(8,4,0), 'W2':(10,5,0), 'W3':(25,6,0), 'W4':(40,7,0),'W5':(74,8,0)}
-# for w in weapons:
-#     print(weapons.get(w))
 
-ring_kombis = [0,1,2,3,4,5,6]
-kombi = itertools.combinations([1,2,3,4,5,6],2)
-for i in kombi: ring_kombis.append(i)
-print (ring_kombis)
+list=[1,2,3,4,5,7,8,9,10,11]
+kombi = itertools.combinations(list,5)
+for i in kombi: print(i)
 
 
 
