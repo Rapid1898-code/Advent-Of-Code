28a23a12a6d97017e727d2549307dd704d826b21
Rapid1898-code <markuspolzer@gmx.at>
2020-01-14 11:10:46 +0100

    kjlkgfhdfh

---
+++ Advent2015/2015Advent24Par1.py
@@ -0,0 +1,44 @@
+import itertools
+
+erg = [1,3,5,11,13,17,19,23,29,31,41,43,47,53,59,61,67,71,73,79,83,89,97,101,103,107,109,113]
+target = 516
+erg.reverse()
+
+arr = []
+for r in range(1,len(erg)-2):
+    found = False
+    for i in itertools.combinations(erg, r):
+        if sum(i) == target:
+            print(i)
+            arr.append(i)
+            found = True
+    if found == True: break
+
+end_min = 9999999999999999999999999
+def prod(it):
+    p = 1
+    for i in it:
+        p *= i
+    return p
+
+for i in arr:
+    end_min = min(end_min, prod(i))
+
+print (len (arr))
+print (end_min)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
