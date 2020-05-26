e0f57cdb99fc80a6e55ddb252ac3e7929bc1103a
Rapid1898-code <57018730+Rapid1898-code@users.noreply.github.com>
2020-05-26 07:19:41 +0200

    More

--- test.py
+++ test.py
@@ -1,23 +1,5 @@
-# mylist = [1,2,3]
-# for i in mylist: print(i)
-#
-# mylist = [x*x for x in range(5)]
-# print(mylist)
-#
-# mylist = [x-1 for x in range(0,6,2)]
-# print(mylist)
-
-# mygenerator =(x*x for x in range(1,5))
-# print(mygenerator)
-# for i in mygenerator: print(i)
-
-def createGenerator(n):
-    mylist = range(n)
-    for i in mylist:
-        yield i*i
-
-mygenrator = createGenerator(3)
-for i in mygenrator: print(i)
+s = "te-st"
+if "te" in s: print("Drinnen")
 
 
 
