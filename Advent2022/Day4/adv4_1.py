049b49fc8e157179030dbf304b6f2dfaf73178de
Markus Polzer <markuspolzer@gmx.at>
2022-12-04 10:38:52 +0100

    upd

---
+++ Advent2022/Day4/adv4_1.py
@@ -0,0 +1,17 @@
+with open("inp.txt","r") as f:
+  listInp = [x.strip() for x in f.readlines()]
+
+countOverlaps = 0
+for e in listInp:
+  worker = e.split(",")
+  from1 = int(worker[0].split("-")[0])
+  to1 = int(worker[0].split("-")[1])
+  from2 = int(worker[1].split("-")[0])
+  to2 = int(worker[1].split("-")[1])
+  if from1 >= from2 and to1 <= to2:
+    # input(e)
+    countOverlaps +=1    
+  elif from2 >= from1 and to2 <= to1:
+    # input(e)
+    countOverlaps +=1
+print(countOverlaps)
