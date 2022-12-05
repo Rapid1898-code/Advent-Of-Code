293451f6d3bc96099ddabe2a7898df484fb57e00
Rapid1898 <rapid1898@gmail.com>
2022-12-05 10:54:35 +0100

    upd

---
+++ Advent2022/Day3/test.py
@@ -0,0 +1,11 @@
+l = [1,5,3,4,2]
+# print(l[::-1])
+# l.sort(reverse=True)
+# print(l)#
+# l.append(9)
+# l.extend([5,6,7])
+# l = list(set(l))
+s = " ".join([str(x) for x in l])
+print(s)
+
+
---
+++ Advent2022/Day5/adv5_2.py
@@ -0,0 +1,63 @@
+with open("inp.txt","r") as f:
+  listInp = [x for x in f.readlines()]
+
+wIDX = listInp.index("\n")
+inp1 = listInp[:wIDX]
+inp2 = listInp[wIDX + 1:]
+
+dictStack = {}
+for idx, r in enumerate(inp1, start=1):
+  if r[1] == "1":
+    break
+  workLine = []
+  for cIDX, cVal in enumerate(r):
+    if (cIDX-1) % 4 == 0: 
+      # print(cIDX, cVal)
+      worker = cVal.split()
+      if len(worker) > 0:
+        worker = worker[0]
+        workLine.append(worker)
+      else: 
+        workLine.append("")
+  for i,e in enumerate(workLine, start=1):
+    if e == "":
+      continue
+    if i not in dictStack:
+      dictStack[i] = [e]
+    else:
+      dictStack[i].append(e)
+dictStack = {k: v for k, v in sorted(dictStack.items(), key=lambda item: item[0])}
+# for k,v in dictStack.items():
+#   print(k,v)
+# exit()
+
+inpMoves = []
+for r in inp2:
+  worker = r.split()
+  worker = [int(x) for x in worker if x.isdigit()]
+  inpMoves.append(worker)
+# for e in inpMoves:
+#   print(e)
+# exit()
+
+def changeItems2 (count, lFrom, lTo):
+  # print(count, lFrom, lTo)
+  worker = lFrom[:count]
+  for e in worker[::-1]:
+    lTo.insert(0,e)
+  lFrom = lFrom[count:]
+  # print(count, lFrom, lTo)
+  # exit()
+  return(lFrom, lTo)
+
+for r in inpMoves:
+  dictStack[r[1]], dictStack[r[2]]= changeItems2(r[0], dictStack[r[1]], dictStack[r[2]])
+  # for k,v in dictStack.items():
+  #   print(k,v)
+  # input("Press!")
+
+finalString = ""
+for k,v in dictStack.items():
+  finalString += v[0]
+
+print(finalString)
