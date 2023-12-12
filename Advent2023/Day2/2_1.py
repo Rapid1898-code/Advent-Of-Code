4cc993eda6d9fd3c6f3469015121efd22563e1ac
Rapid1898 <rapid1898@gmail.com>
2023-12-12 19:21:31 +0100

    upd

---
+++ Advent2023/Day2/2_1.py
@@ -0,0 +1,40 @@
+import os 
+import sys
+import string
+
+if __name__ == '__main__':
+  path = os.path.abspath(os.path.dirname(sys.argv[0])) 
+  fn = os.path.join(path, "inp.txt")
+  with open(fn, encoding="utf8", errors="ignore") as f:
+    lines = f.read().splitlines()
+
+  inpDict = {}
+  for l in lines:
+    wGameNr = int(l.split()[1].replace(":",""))
+    worker = l.split()[2:]
+
+    dictVal = {}
+    for i,e in enumerate(worker):
+      if i % 2 == 0:
+        checkVal = worker[i+1].replace(",","").replace(";","")
+        if checkVal not in dictVal:
+          dictVal[checkVal] = int(e)
+        else:
+          dictVal[checkVal] += int(e)         
+        # print(dictVal)
+        # input("Press!")
+    inpDict[wGameNr] = dictVal
+
+  countSummary = 0
+  for k, v in inpDict.items():
+    if v["red"] <= 12 \
+      and v["green"] <= 13 \
+      and v["blue"] <= 14:
+        countSummary += k
+    print(k, v)
+    print(countSummary)
+    input("Press!")
+
+
+
+  print(countSummary)
